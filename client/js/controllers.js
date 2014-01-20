391bef47fa2dc6418c6079bb57c960d3ec91cf7d
yuanfeng feng <harry.yuanfeng@gmail.com>
2014-01-21 07:34:24 +0800

    add dollar sign to state

--- client/js/controllers.js
+++ client/js/controllers.js
@@ -3,14 +3,14 @@
 var controllers = angular.module('manager.controllers', []);
 
 controllers.controller('NavCtrl', ['$state', 'TabItems', 'UserProfile',
-function(state, tabItems, userProfile) {
+function($state, tabItems, userProfile) {
 	this.tabItems = angular.copy(tabItems);
 
 	this.userProfile = angular.copy(userProfile);
 
 	// Determin whether the current tab is active or not
 	this.isActive = function(sref) {
-		return (state.current.name.search(sref) !== -1) ? true : false;
+		return ($state.current.name.search(sref) !== -1) ? true : false;
 	};
 }]);
 
