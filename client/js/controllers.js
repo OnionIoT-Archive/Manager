b22f9e2fdc5529ea85f577e1d0a09a77992cfb6f
yuanfeng feng <harry.yuanfeng@gmail.com>
2014-01-21 07:26:17 +0800

    user authentication without session

--- client/js/controllers.js
+++ client/js/controllers.js
@@ -2,13 +2,38 @@
 
 var controllers = angular.module('manager.controllers', []);
 
-controllers.controller('NavCtrl', ['$state', 'TabItems', 'UserProfile', function (state, tabItems, userProfile) {
+controllers.controller('NavCtrl', ['$state', 'TabItems', 'UserProfile',
+function(state, tabItems, userProfile) {
 	this.tabItems = angular.copy(tabItems);
 
 	this.userProfile = angular.copy(userProfile);
 
 	// Determin whether the current tab is active or not
-	this.isActive = function (sref) {
+	this.isActive = function(sref) {
 		return (state.current.name.search(sref) !== -1) ? true : false;
 	};
+}]);
+
+//the controller for the socket
+controllers.controller('AppLoginCtrl', ['$scope', 'socket',
+function($scope, socket) {
+	var self = this;
+	socket.on('test', function(data) {
+		self.test = data.data;
+	});
+	socket.on('isLogin', function(data) {
+		if (data.status) {
+			self.status = "succsss!";
+		} else {
+			self.status = "fail please try again!";
+		}
+	});
+	this.authen = function(email, password) {
+		console.log(email);
+		console.log(password);
+		socket.emit('login', {
+			email : email,
+			password : password
+		});
+	}
 }]);
--- main.js
+++ main.js
@@ -12,26 +12,44 @@ var expressServer = express();
 var httpServer = http.createServer(expressServer);
 var socketServer = socket.listen(httpServer);
 
-
 /***** WebSocket server *****/
 
-socketServer.sockets.on('connection', function (socket) {
-	socket.emit('news', { hello: 'world' });
-	socket.on('my other event', function (data) {
+socketServer.sockets.on('connection', function(socket) {
+	socket.emit('news', {
+		hello : 'world'
+	});
+	socket.emit('test', {
+		data : 'socket io works'
+	});
+	socket.on('my other event', function(data) {
 		console.log(data);
+	});
+	socket.on('login', function(data) {
+		console.log(data);
+		var emialAccount = "harry@onion.io";
+		var password = "success";
+		if (data.email == emialAccount && data.password == password) {
+			socket.emit('isLogin', {
+				status : true
+			});
+		} else {
+			socket.emit('isLogin', {
+				status : false
+			});
+		}
 	});
 });
 
 /***** HTTP server *****/
 
 // Configure the express server
-expressServer.configure(function () {
+expressServer.configure(function() {
 	expressServer.use(express.basicAuth('dev', 'philosophy'));
-    expressServer.use('/', express.static(__dirname + '/client'));
+	expressServer.use('/', express.static(__dirname + '/client'));
 
-    expressServer.get('*', function (req, res) {
+	expressServer.get('*', function(req, res) {
 		res.redirect('/');
 	});
 });
 
-httpServer.listen(80);
+httpServer.listen(8081);
