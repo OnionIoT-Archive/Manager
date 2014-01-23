9b939eedcfc46ee05833bb3c304f352db7f7d512
yuanfeng feng <harry.yuanfeng@gmail.com>
2014-01-23 11:36:51 +0800

    add socket on LOGIN_FAIL, LOGIN_SUCCESS, SIGNUP_FAIL, SIGNUP_SUCCESS

--- main.js
+++ main.js
@@ -6,6 +6,7 @@ var socket = require('socket.io');
 var http = require('http');
 var manager = require('./server/main');
 var rpc = require('./server/amqp-rpc/amqp_rpc');
+var nodemailer = require("nodemailer");
 
 // Create servers
 var expressServer = express();
@@ -16,6 +17,15 @@ expressServer.use(express.cookieParser());
 expressServer.use(express.session({
 	secret : 'onion.io'
 }));
+
+// create reusable transport method (opens pool of SMTP connections)
+var smtpTransport = nodemailer.createTransport("SMTP", {
+	service : "Gmail",
+	auth : {
+		user : "harry@onion.io",
+		pass : "Aa!123456"
+	}
+});
 
 rpc.call('DB_CHECK_TEST', {
 }, function(result) {
@@ -36,41 +46,44 @@ rpc.call('DB_ADD_USER', {
 });
 
 rpc.call('DB_ADD_DEVICE', {
-	name:'harry friday',
+	name : 'harry friday',
 }, function(result) {
 	console.log('DB_ADD_DEVICE');
 	console.log(result);
 });
 
 rpc.call('DB_GET_USER', {
-	email : 'harry@onion.io'
+	email : 'harry@onion.io2'
 }, function(result) {
 	console.log('DB_GET_USER');
 	console.log(result);
 });
 
 rpc.call('DB_GET_DEVICE', {
-	name:'harry friday'
+	name : 'harry friday'
 }, function(result) {
 	console.log('DB_GET_DEVICE harry');
 	console.log(result);
 });
 
 rpc.call('DB_DELETE_DEVICE', {
-	_id:'52e03edfb2547c00005b8a27'
+	_id : '52e03edfb2547c00005b8a27'
 }, function(result) {
 	console.log('DB_DELETE_DEVICE harry');
 	console.log(result);
 });
 
 rpc.call('DB_UPDATE_DEVICE', {
-	condition:{_id:"52e03f8001797800006574c3"},
-	update:{name:"update harry friday"}
+	condition : {
+		_id : "52e03f8001797800006574c3"
+	},
+	update : {
+		name : "update harry friday"
+	}
 }, function(result) {
 	console.log('DB_UPDATE_DEVICE harry');
 	console.log(result);
 });
-
 
 /***** WebSocket server *****/
 
@@ -81,13 +94,11 @@ socketServer.sockets.on('connection', function(socket) {
 	socket.emit('test', {
 		data : 'socket io works'
 	});
-	socket.on('LOGIN', function(data) {
-		var emialAccount = "harry@onion.io";
-		var password = "success";
 
-		rpc.call('DB_CHECK_USER', {
-			user : 'guest',
-			pass : 'guest'
+	socket.on('LOGIN', function(data) {
+		rpc.call('DB_GET_USER', {
+			email : data.email,
+			passHash : data.hash
 		}, function(result) {
 			if (result) {
 				socket.emit('LOGIN_SUCCESS', {
@@ -98,6 +109,38 @@ socketServer.sockets.on('connection', function(socket) {
 			}
 		});
 	});
+
+	socket.on('SIGNUP', function(data) {
+
+		rpc.call('DB_GET_USER', {
+			email : data.email
+		}, function(result) {
+			if (result) {
+				socket.emit('SIGNUP_FAIL', {
+				});
+			} else {
+				rpc.call('DB_ADD_USER', {
+					email : data.email,
+					passHash : data.hash
+				}, function(result) {
+					socket.emit('SIGNUP_SUCCESS', {
+					});
+				});
+			}
+		});
+	});
+
+	socket.on('SIGNUP', function(data) {
+		// setup e-mail data with unicode symbols
+		var mailOptions = {
+			from : "Onion ✔ <harry@onion.io>", // sender address
+			to : data.email, // list of receivers
+			subject : "Onion:Reset Passwrd", // Subject line
+			text : "Http://www.onion.io/changethis", // plaintext body
+			html : "<b>Click <a href='#'>here</a> to reset your password</b>" // html body
+		}
+	});
+
 });
 
 /***** HTTP server *****/
